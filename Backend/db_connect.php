a4b3216c3437dd9843a494dad67aa14ec9a4a100
Aymar <yannyao4066@gmail.com>
2023-02-24 01:00:12 +0000

    angular-php-crud

---
+++ Backend/db_connect.php
@@ -0,0 +1,20 @@
+<?php
+class Operations
+{
+    private $db_host = 'localhost';
+    private $db_name = 'devooti';
+    private $db_username = 'postgres';
+    private $db_password = 12136270;
+ 
+    public function dbConnection()
+    { 
+        try {
+            $conn = new PDO('pgsql:host=' . $this->db_host . ';dbname=' . $this->db_name, $this->db_username, $this->db_password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch (PDOException $e) {
+            echo "Connection error " . $e->getMessage();
+            exit;
+        }
+    }
+}
